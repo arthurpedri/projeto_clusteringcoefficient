4e28709b327141a67b9b672ccd26040142d83b3f
Strozzi <laps15@inf.ufpr.br>
2019-03-17 23:29:14 +0100

    falta o caclculo do coeficiente

--- grafo.c
+++ grafo.c
@@ -24,12 +24,15 @@ struct vertice{
 
 vertice adc_vetice(char* nome, grafo g);
 
+int busca_aresta(vertice v, char *nome);
+
 vertice adc_vetice(char* nome, grafo g){
     size_t nome_size = strlen(nome);
     if (nome == NULL || nome_size == 0) {
         return NULL;
     }
     vertice novo_vertice = NULL;
+    g->tamanho++;
     for (vertice i = g->vertices; i != NULL; i = i->prox) {
         if (strcmp(nome, i->nome) == 0) {
             novo_vertice = i;
@@ -46,7 +49,17 @@ vertice adc_vetice(char* nome, grafo g){
     printf("%s\n",nome );
     return(novo_vertice);
 }
-
+//retorna 1 se achou, 0 se não achou
+int busca_aresta(vertice v, char *nome){
+    aresta *a = v->lista;
+    while (a != NULL) {
+        if (strcmp(nome, a->vizinho->nome) == 0 ) {
+            return(1);
+        }
+        a = a->prox;
+    }
+    return(0);
+}
 grafo le_grafo(FILE *input){
   grafo novo_grafo = NULL;
   char token[1025];
@@ -57,6 +70,7 @@ grafo le_grafo(FILE *input){
   novo_grafo =(grafo) malloc(sizeof(struct grafo));
   novo_grafo->vertices = NULL;
   novo_grafo->nome= NULL;
+  novo_grafo->tamanho = 0;
 
   //lê linhas pegando os token=palavras
   while(fscanf(input, "%s%c", token, &separador) > 0){
@@ -65,15 +79,16 @@ grafo le_grafo(FILE *input){
     // se tem mais de uma string na linhas adc aresta
       fscanf(input, "%s%c", token, &separador);
       vertice v2 = adc_vetice(token, novo_grafo);
-
-      aresta *aux = malloc(sizeof(struct aresta));
-      aux->prox = v1->lista;
-      aux->vizinho = v2;
-      v1->lista = aux;
-      aux = malloc(sizeof(struct aresta));
-      aux->prox = v2->lista;
-      v2->lista = aux;
-      aux->vizinho = v1;
+      if(!(busca_aresta(v1, token))){
+          aresta *aux = malloc(sizeof(struct aresta));
+          aux->prox = v1->lista;
+          aux->vizinho = v2;
+          v1->lista = aux;
+          aux = malloc(sizeof(struct aresta));
+          aux->prox = v2->lista;
+          v2->lista = aux;
+          aux->vizinho = v1;
+      }
     }
   }
   return(novo_grafo);
@@ -124,7 +139,7 @@ grafo escreve_grafo(FILE *output, grafo g){
         vprox = v->prox;
     //print vertices
     while (v != NULL) {
-        fprintf(output, "\t%s:\n\t\t",v->nome);
+        fprintf(output, "\t%s:",v->nome);
         aresta *a = v->lista;
         aresta *aprox = NULL;
         if (a != NULL)
@@ -145,7 +160,10 @@ grafo escreve_grafo(FILE *output, grafo g){
 }
 
 double coeficiente_agrupamento_grafo(grafo g){
-
+// 3* nro de triangulos /nro de trios
+    int t_abertas = 1;
+    int t_fechadas = 0;
+    
 
 
 
