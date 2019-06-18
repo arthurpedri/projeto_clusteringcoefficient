2132536e089b585b9468ddeba7acbd7a1e4debc7
Arthur Pedri <arthur.pedri@gmail.com>
2019-06-18 17:27:15 -0300

    T3

--- grafo.h
+++ grafo.h
@@ -5,23 +5,23 @@
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um grafo
-//
+// 
 // o grafo tem um nome, que é uma "string"
 
 typedef struct _grafo *grafo;
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um vértice
-//
+// 
 // o vértice tem um nome, que é uma "string"
 
 typedef struct _vertice *vertice;
 
 //------------------------------------------------------------------------------
 // desaloca toda a memória usada em *g
-//
+// 
 // devolve 1 em caso de sucesso,
-//         ou
+//         ou 
 //         0, caso contrário
 
 int destroi_grafo(grafo g);
@@ -30,18 +30,18 @@ int destroi_grafo(grafo g);
 // lê um grafo de input.
 // um grafo é um arquivo onde cada linha tem zero, uma ou duas
 // strings (sequência de caracteres sem "whitespace").
-//
+// 
 // devolve o grafo lido. Caso o arquivo esteja mal formado o
 // comportamento da função é indefinido
 
-grafo le_grafo(FILE *input);
+grafo le_grafo(FILE *input);  
 
 //------------------------------------------------------------------------------
 // escreve o grafo g em output, no mesmo formato que o usado por le_grafo()
 //
 // devolve o grafo escrito,
-//         ou
-//         NULL, em caso de erro
+//         ou 
+//         NULL, em caso de erro 
 
 grafo escreve_grafo(FILE *output, grafo g);
 
@@ -55,42 +55,51 @@ unsigned int n_vertices(grafo g);
 
 unsigned int n_arestas(grafo g);
 
+//..............................................................................
+// as funções primeiro_vertice() e proximo_vertice() a seguir
+// implementam um iterador para os vertices de g de maneira que, por
+// exemplo, o laço
+//
+//    for (v=primeiro_vertice(g); v; v=proximo_vertice(v,g)) {
+//        processa(v);
+//        }
+//
+// executa processa(v) uma vez para cada vertice v de g
+//..............................................................................
+
 //------------------------------------------------------------------------------
-// devolve o vértice de nome 'nome' em g
-//         ou
-//         NULL, caso não exista tal vertice
-
-vertice vertice_nome(grafo g, char *nome);
-
-//------------------------------------------------------------------------------
-// devolve o nome de v
-
-char *nome(vertice v);
+// devolve o primeiro vertice de g numa iteração por todos os vertices de g 
+//      ou
+//         NULL, se g não tem vertices
+ 
+vertice primeiro_vertice(grafo g);
 
 //------------------------------------------------------------------------------
-// devolve o número mínimo k de trilhas necessárias para cobrir o grafo g
-//
-// observe que
-//
-//   k = 1, se g é euleriano, ou
-//   k = (número de vértices de grau ímpar em g)/2, caso contrário
-//
-// aloca e preenche o vetor cobertura de maneira que cobertura[i] contém uma
-// trilha em G para cada 0 <= i < k.
-//
-// cada trilha é um sequência de vertices terminada por NULL, isto é, para cada 0 <= i < k
-// a sequência
-//
-//     (cobertura[i][0], cobertura[i][1], ..., cobertura[i][l-1], cobertura[i][l])
-//
-// é tal que
-//
-//     1. cobertura[i][j] é um vertice, para cada 0 <= j < l,
-//     2. cobertura[i][l] == NULL, e
-//     3. cobertura[i][j-1] é vizinho de cobertura[i][j] em g, para cada 0 < j < l
+// devolve o vertice seguinte a v em g numa iteração por todos os vertices de g 
+//      ou
+//         NULL, se v é o último vertice da iteração
 
-unsigned int cobertura_por_trilhas(grafo g, vertice **cobertura[]);
+vertice proximo_vertice(vertice v, grafo g);
 
-double coeficiente_agrupamento_grafo(grafo g);
+//------------------------------------------------------------------------------
+// computa um emparelhamento de tamanho máximo no grafo bipartido g e devolve o
+// tamanho (número de arestas) do emparelhamento computado
+
+unsigned int emparelha(grafo g);
+
+//------------------------------------------------------------------------------
+// devolve 
+//         NULL se v é descoberto pelo emparelhamento M,
+//      ou
+//         o par de v no emparelhamento M, 
+//         isto é, 
+//         a outra ponta da aresta de M que cobre v, caso contrário,
+//
+// onde M é
+//         o emparelhamento computado pela última execução de emparelha(g),
+//       ou
+//         o emparelhamento vazio, se emparelha(g) não foi executado
+
+vertice par(vertice v);
 //------------------------------------------------------------------------------
 #endif
